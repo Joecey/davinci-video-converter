1eaf3bee417a8257a71efa21ccfa21a02a03838f
Joecey <joe.linogao@gmail.com>
2025-10-08 20:27:12 +0100

    converter works

--- go.mod
+++ go.mod
@@ -5,10 +5,12 @@ go 1.25.1
 require (
 	github.com/charmbracelet/huh v0.7.0
 	github.com/schollz/progressbar/v3 v3.18.0
+	github.com/u2takey/ffmpeg-go v0.5.0
 )
 
 require (
 	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/aws/aws-sdk-go v1.38.20 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/catppuccin/go v0.3.0 // indirect
 	github.com/charmbracelet/bubbles v0.21.0 // indirect
@@ -21,6 +23,7 @@ require (
 	github.com/charmbracelet/x/term v0.2.1 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
@@ -31,6 +34,7 @@ require (
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/termenv v0.16.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/u2takey/go-utils v0.3.1 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/sync v0.12.0 // indirect
 	golang.org/x/sys v0.37.0 // indirect
--- utils/conversion/conversion.go
+++ utils/conversion/conversion.go
@@ -2,19 +2,63 @@ package conversion
 
 import (
 	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/schollz/progressbar/v3"
+	ffmpeg "github.com/u2takey/ffmpeg-go"
 )
 
 func Convert (originPath string, targetPath string)(error){
-	files,_ := os.ReadDir(originPath)
+	files,err := os.ReadDir(originPath)
+	if err != nil {
+        return err
+    }
+
+	if _, err := os.Stat(targetPath); os.IsNotExist(err) {
+		err = os.MkdirAll(targetPath, 0755)
+		if err != nil {
+			return err
+		}
+	}
 
 	numFiles := len(files)
 	bar := progressbar.Default(int64(numFiles))
-	for i := 0; i < numFiles; i++ {
-		bar.Add(1)
-		time.Sleep(40 * time.Millisecond)
-	}
-	return nil
+
+	for _, file := range files {
+        if file.IsDir() {
+            continue
+        }
+
+		if strings.ToLower(filepath.Ext(file.Name())) != ".mp4"{
+			continue
+		}
+
+        inputFile := filepath.Join(originPath, file.Name())
+        // Change extension to .mov for output
+        outputFileName := strings.TrimSuffix(file.Name(), filepath.Ext(file.Name())) + ".mov"
+        outputFile := filepath.Join(targetPath, outputFileName)
+
+        err := ffmpeg.Input(inputFile).
+            Output(outputFile, ffmpeg.KwArgs{
+                "c:v": "mjpeg",
+                "q:v": "2",
+                "c:a": "pcm_s16be",
+                "q:a": "0",
+                "f":   "mov",
+				"loglevel": "quiet", 
+            }).
+            OverWriteOutput().
+			WithOutput().
+			Run()
+
+        if err != nil {
+            return err
+        }
+
+        bar.Add(1)
+        time.Sleep(40 * time.Millisecond) // Keep for smooth bar update
+    }
+    return nil
 }	
